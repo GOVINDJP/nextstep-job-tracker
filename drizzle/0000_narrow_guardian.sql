5d1bd5374b35364353bee98a1636f2eace2c09ea
GOVINDJP <govindjayakumar34@gmail.com>
2026-09-22 13:28:26 +0530

    Add drizzle/0000_narrow_guardian.sql

---
+++ drizzle/0000_narrow_guardian.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `applications` (
+	`id` text PRIMARY KEY NOT NULL,
+	`company` text NOT NULL,
+	`role` text NOT NULL,
+	`stage` text NOT NULL,
+	`interview` text DEFAULT '' NOT NULL,
+	`notes` text DEFAULT '' NOT NULL,
+	`created` text NOT NULL
+);
+
